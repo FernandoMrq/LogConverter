c914c80b08efd2b152b20cdcbfee6404a3a54526
Fernando Marques <frndoliveira@gmail.com>
2022-09-06 08:53:39 -0300

    partial design

--- CandidateTesting.FernandoMarques/Consoles/LogConverter.cs
+++ CandidateTesting.FernandoMarques/Consoles/LogConverter.cs
@@ -6,7 +6,45 @@ namespace CandidateTesting.FernandoMarques.Consoles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (StayOpen())
+            {
+                Clear();
+                var url = GetURL();
+            }
+
+            Console.WriteLine("Não está pronto");
         }
+
+        //Inerente ao console
+        private static bool StayOpen()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Precisa existir? | Inerente ao console
+        private static string GetURL()
+        {
+            string url = "";
+
+            //Tem de ficar aqui?
+            if (!ValidaUrl(url))
+                throw new Exception("URL not valid");
+
+            throw new NotImplementedException();
+        }
+
+        //Inerente a regra
+        private static bool ValidaUrl(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Precisa existir? | Inerente ao console
+        private static void Clear()
+        {
+            Console.Clear();
+        }
+
+        //private static void 
     }
 }
